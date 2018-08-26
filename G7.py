153ca547f0d7c0ed7a90a41c8d584bd3569535e1
alexkaixianxu <42716114+alexkaixianxu@users.noreply.github.com>
2018-08-26 10:42:07 -0400

    Alex
    Download G7 currency price data

---
+++ G7.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import quandl
+import datetime
+from scipy.stats import norm
+quandl.ApiConfig.api_key = '8qNu5k13WLNT-6tRKj9M'
+
+def data_download(start='2007-01-01',end=datetime.date.today()):
+    frame_ticker = ['EURUSD', 'EURUSD', 'EURUSD', 'USDJPY', 'USDJPY', 'USDJPY', 'GBPUSD', 'GBPUSD', 'GBPUSD', 'USDCAD',
+                    'USDCAD', 'USDCAD', 'AUDUSD', 'AUDUSD', 'AUDUSD', 'NZDUSD', 'NZDUSD', 'NZDUSD']
+    ohlc = ['Rate', 'High (est)', 'Low (est)']
+    G7 = pd.DataFrame(columns=[frame_ticker, ohlc * 6])
+    for ticker, x in zip(frame_ticker, ohlc * 6):
+        G7[(ticker, x)] = quandl.get('CURRFX/{}'.format(ticker), start_date=start, end_date=end)[x]
+    usd_index=quandl.get('FRED/DTWEXM', start_date=start,end_date=G7.tail(1).index)
+    G7['USD']=usd_index
+    return G7.dropna(axis=0)
